9654994c622b18e3287a829f7010aca41d62fa56
Piyush Ranjan <piyush19jan@gmail.com>
2016-12-30 01:11:54 +0530

    Completed test autoamtion
     - TODO : Run once live call starts working

--- client/PlivoClient.py
+++ client/PlivoClient.py
@@ -1,11 +1,15 @@
 import requests
 import json
 import os
+import re
 
 class PlivoClient():
 
-    search_url = "https://api.plivo.com/v1/Account/MANJRIOTNJOTNMNZC1OD/PhoneNumber/?country_iso=US"
+    search_url = "https://api.plivo.com/v1/Account/{auth_id}/PhoneNumber/?country_iso=US"
     buy_url = "https://api.plivo.com/v1/Account/{auth_id}/PhoneNumber/{number}/"
+    call_url = "https://api.plivo.com/v1/Account/{auth_id}/Call/"
+    live_calls_url = "https://api.plivo.com/v1/Account/{auth_id}/Call/?status=live"
+    live_call_details_url = "https://api.plivo.com/v1/Account/{auth_id}/Call/{call_uuid}/?status=live"
 
     def __init__(self):
         fileDir = os.path.dirname(os.path.realpath('__file__'))
@@ -17,8 +21,8 @@ class PlivoClient():
     def set_numbers(self, numbers):
         global number1
         global number2
-        number1= numbers[0]
-        number2 = numbers[1]
+        number1 = "18883964261"
+        number2 = "18883424094"
 
     def get_numbers(self, number):
         if number == 1:
@@ -27,7 +31,8 @@ class PlivoClient():
             return number2
 
     def send_search_number_request(self, headers):
-        response = requests.get(self.search_url, headers=headers)
+        search_url = self.search_url.replace("{auth_id}", self.data['auth_id'])
+        response = requests.get(search_url, headers=headers)
         print(response.status_code)
         print(response.json)
         return response
@@ -44,8 +49,6 @@ class PlivoClient():
             assert number_object['region'] == 'United States'
             assert len(number_object['resource_uri']) > 0
         self.set_numbers(numbers)
-        print(self.get_numbers(1))
-        print(self.get_numbers(2))
         return data_dict
 
     def get_data_for_call_api(self, response):
@@ -57,10 +60,75 @@ class PlivoClient():
         buy_url2 = self.buy_url.replace("{number}", self.get_numbers(2)).replace("{auth_id}", self.data['auth_id'])
         response1 = requests.post(buy_url1, headers=headers)
         response2 = requests.post(buy_url2, headers=headers)
-        print(response1.status_code)
-        print(response1.json())
-        print(response2.status_code)
-        print(response2.json())
+        resp_dict = {"response1": response1, "response2": response2}
+        return resp_dict
 
     def verify_buy_number_response(self, response):
+        pattern = "^[a-zA-Z0-9-]*$"
+        assert response['response1'].status_code == 201
+        assert response['response2'].status_code == 201
+        resp_data1 = json.loads(response['response1'])
+        resp_data2 = json.loads(response['response1'])
+        assert resp_data1['message'] == 'created'
+        assert resp_data1['status'] == 'fulfilled'
+        assert resp_data1['numbers'][0]['number'] == self.get_numbers(1)
+        assert resp_data1['numbers'][0]['status'] == 'Success'
+        assert re.match(pattern, resp_data1['api_id'])
+        assert resp_data2['message'] == 'created'
+        assert resp_data2['status'] == 'fulfilled'
+        assert resp_data2['numbers'][0]['number'] == self.get_numbers(2)
+        assert resp_data2['numbers'][0]['status'] == 'Success'
+        assert re.match(pattern, resp_data2['api_id'])
+
+    def make_outbound_call_request(self, headers):
+        call_url = self.call_url.replace("{auth_id}", self.data['auth_id'])
+        payload = {}
+        payload['from'] = "18883964261"
+        payload['to'] = "18883424094"
+        payload['answer_url'] = "https://s3.amazonaws.com/plivosamplexml/speak_url.xml"
+        payload_json = json.dumps(payload)
+        response = requests.post(call_url, data=payload_json, headers=headers)
+        return response
+
+    def verify_make_outboud_call_response(self, response):
+        pattern = "^[a-zA-Z0-9-]*$"
         assert response.status_code == 201
+        resp_data = json.loads(response)
+        assert resp_data['message'] == 'call fired'
+        assert re.match(pattern, resp_data['request_uuid'])
+
+    def get_ongoing_live_calls(self, headers):
+        live_call_url = self.live_calls_url.replace("{auth_id}", self.data['auth_id'])
+        response = requests.get(live_call_url, headers=headers)
+        return response
+
+    def verify_ongoing_live_call_response(self, response):
+        global call_uuid
+        assert response.status_code == 200
+        resp_data = json.loads(response)
+        assert len(resp_data['calls']) > 0
+        call_uuid = resp_data['calls'][0]
+        print(call_uuid)
+
+    def get_live_call_details(self, headers):
+        live_call_detail_url =self.live_call_details_url.replace("{auth_id}", self.data['auth_id']).replace("{call_uuid}", call_uuid)
+        print(live_call_detail_url)
+        response = requests.get(live_call_detail_url, headers=headers)
+        return response
+
+    def verify_live_call_details_response(self, response):
+        pattern = "^[a-zA-Z0-9-]*$"
+        assert response.status_code == 200
+        resp_data = json.loads(response)
+        assert resp_data['direction'] == 'inbound'
+        assert resp_data['from'] == self.get_numbers(1)
+        assert resp_data['to'] == self.get_numbers(2)
+        assert resp_data['call_status'] == 'in-progress'
+        assert re.match(pattern, resp_data['api_id'])
+        assert resp_data['caller_name'] == "+" + self.get_numbers(1)
+        assert re.match(pattern, resp_data['call_uuid'])
+
+
+
+
+
--- test/PlivoTest.py
+++ test/PlivoTest.py
@@ -15,12 +15,10 @@ class PlivoTest(unittest.TestCase):
         headers = {
             "Authorization": "Basic TUFOSlJJT1ROSk9UTk1OWkMxT0Q6TkRWbU9UUTVNVFkyTW1Vek4yTmtPVFU0TW1SbVlqQXhNelV6TUdabA=="}
         resp = client.send_search_number_request(headers)
-        data = client.get_data_for_call_api(resp.content)
-        api_id = data['api_id']
-        number_details = data['objects'][1:2]
         client.verify_search_number_request(resp)
+        print("################################ Completed test for search number ################################")
 
-    @test()
+    @test(depends_on=[send_search_request])
     def buy_number_test():
         client = PlivoClient.PlivoClient()
         print("################################ Starting test for buy number ################################")
@@ -28,5 +26,36 @@ class PlivoTest(unittest.TestCase):
             "Authorization": "Basic TUFOSlJJT1ROSk9UTk1OWkMxT0Q6TkRWbU9UUTVNVFkyTW1Vek4yTmtPVFU0TW1SbVlqQXhNelV6TUdabA==", "Content-Type": "application/json"}
         resp = client.send_buy_number_request(headers)
         client.verify_buy_number_response(resp)
+        print("################################ Ended test for buy number ################################")
+
+    @test(depends_on=[buy_number_test])
+    def make_outbound_call_test():
+        print("################################ Starting test for making outbound calls ################################")
+        client = PlivoClient.PlivoClient()
+        headers = {
+            "Authorization": "Basic TUFOSlJJT1ROSk9UTk1OWkMxT0Q6TkRWbU9UUTVNVFkyTW1Vek4yTmtPVFU0TW1SbVlqQXhNelV6TUdabA==", "Content-Type": "application/json"}
+        response = client.make_outbound_call_request(headers)
+        client.verify_make_outboud_call_response(response)
+        print("################################ Ended test for making outbound calls ################################")
+
+    @test(depends_on=[make_outbound_call_test])
+    def make_get_live_call_test():
+        print("################################ Starting test for getting live calls ################################")
+        client = PlivoClient.PlivoClient()
+        headers = {
+            "Authorization": "Basic TUFOSlJJT1ROSk9UTk1OWkMxT0Q6TkRWbU9UUTVNVFkyTW1Vek4yTmtPVFU0TW1SbVlqQXhNelV6TUdabA=="}
+        response = client.get_ongoing_live_calls(headers)
+        client.verify_ongoing_live_call_response(response)
+        print("################################ Ended test for getting live calls ################################")
+
+    @test(depends_on=[make_get_live_call_test])
+    def make_get_live_call_details_test():
+        print("################################ Starting test for getting live call detials ################################")
+        client = PlivoClient.PlivoClient()
+        headers = {
+            "Authorization": "Basic TUFOSlJJT1ROSk9UTk1OWkMxT0Q6TkRWbU9UUTVNVFkyTW1Vek4yTmtPVFU0TW1SbVlqQXhNelV6TUdabA=="}
+        response = client.get_live_call_details(headers)
+        client.verify_live_call_details_response(response)
+        print("################################ Ended test for getting live call detials ################################")
 
 TestProgram().run_and_exit()
